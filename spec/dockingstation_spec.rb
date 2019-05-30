8ac3a55e02b8c575840fe9b543335ba09e02dcf1
joseph townshend <joetownshend@mac.com>
2019-05-30 14:34:08 +0100

    Q9 bike spec

--- spec/dockingstation_spec.rb
+++ spec/dockingstation_spec.rb
@@ -1,6 +1,7 @@
 require 'docking_station'
+
 describe DockingStation do
   it "responds to release_bike" do
-    expect(DockingStation).to respond_to :release_bike
+    expect(subject).to respond_to :release_bike
   end
 end
