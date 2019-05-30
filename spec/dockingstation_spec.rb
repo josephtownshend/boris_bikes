8d1eac310989a69c4b464e99063bbde80cd6ca5f
joseph townshend <joetownshend@mac.com>
2019-05-30 16:06:39 +0100

    q12

--- spec/dockingstation_spec.rb
+++ spec/dockingstation_spec.rb
@@ -1,10 +1,16 @@
 require 'docking_station'
+require 'bike'
 
 describe DockingStation do
   it { is_expected.to respond_to :release_bike }
 
-  it 'releases working bikes' do
-    bike = subject.release_bike
-    expect(bike).to be_working
+  it { is_expected.to respond_to(:dock).with(1).argument }
+
+  it { is_expected.to respond_to(:bike) }
+
+    it 'returns docked bikes' do
+      bike = Bike.new
+      subject.dock(bike)
+      expect(subject.bike).to eq bike
+    end
   end
-end
